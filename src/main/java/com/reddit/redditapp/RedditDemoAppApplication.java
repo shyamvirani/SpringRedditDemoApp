9b44a894b7f549d2f71efdf7059009d8bcb12516
shyam virani <shyamvirani17@gmail.com>
2020-06-20 15:02:07 +0530

    ok

---
+++ src/main/java/com/reddit/redditapp/RedditDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.reddit.redditapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	
+	@NotBlank(message="username is required")
+	private String username;
+	
+	@NotBlank(message="password is required")
+	private String password;
+	
+	@Email
+	@NotEmpty(message="email is required")
+	private String email;
+	private Instant created;
+	private boolean enabled;
+	
+	
+	
+	
+
+}
