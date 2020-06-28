699aff59591f3d53f4c0fa4b9a1977982fcca7d8
shyam virani <shyamvirani17@gmail.com>
2020-06-28 19:32:58 +0530

    Updated

--- src/main/java/com/reddit/redditapp/RedditDemoAppApplication.java
+++ src/main/java/com/reddit/redditapp/RedditDemoAppApplication.java
@@ -2,10 +2,14 @@ package com.reddit.redditapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.reddit.redditapp.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditDemoAppApplication {
 
 	public static void main(String[] args) {
